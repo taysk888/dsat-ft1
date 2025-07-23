2c328a833374c9319eab8dea057540eb3d674176
taysk888 <102202742+taysk888@users.noreply.github.com>
2025-07-23 07:24:16 +0000

    db2

--- app.py
+++ app.py
@@ -160,6 +160,7 @@ def add_log():
     conn.close()
     return render_template("add_log.html", message="User log added successfully.")
 
+
 @app.route("/delete_log",methods=["GET","POST"])
 def delete_log():
     conn = sqlite3.connect("user.db")
